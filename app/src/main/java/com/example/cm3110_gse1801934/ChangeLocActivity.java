8fafda51a86ed256d58ceb7a0e5817e4c6b1e790
Dvagoes <43369743+Dvagoes@users.noreply.github.com>
2020-10-23 11:47:57 +0100

    Initial Commit

--- app/src/main/java/com/example/cm3110_gse1801934/ChangeLocActivity.java
+++ app/src/main/java/com/example/cm3110_gse1801934/ChangeLocActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LiveTrackActivity extends AppCompatActivity {
+public class ChangeLocActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_live_track);
+        setContentView(R.layout.activity_change_loc);
     }
 }
